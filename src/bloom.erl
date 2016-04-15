fc7cabe650e69e7147d8b842fb8218010d6ab38a
Sebastian Green-Husted <ricecake@tfm.nu>
2016-04-15 18:59:44 -0400

    apply some more optimizations

--- src/bloom.erl
+++ src/bloom.erl
@@ -8,8 +8,7 @@
 	exists/2,
 	optimal_params/2,
 	setBits/2,
-	checkBits/2,
-	compute_deltas/3
+	getBit/2
 ]).
 
 -ifdef(TEST).
@@ -38,14 +37,13 @@ optimal_params(Elements, Odds) ->
 	{ok, {Width, Hashes}}.
 
 add({bloom_state, State, Width, Rounds}, Data) when is_binary(Data) ->
-	%NewState = setBits(State, compute_deltas(lists:sort(hash_bits(Width, Data, lists:seq(1, Rounds)))),
-	NewState = do_add(State, Data, Width, Rounds),
+	NewState = setBits(State, hash_bits(Width, Data, lists:seq(1, Rounds))),
 	{bloom_state, NewState, Width, Rounds};
 add(State, Data) when not is_binary(Data) ->
 	add(State, term_to_binary(Data)).
 
 
-exists({bloom_state, State, Width, Rounds}, Data) ->
+exists({bloom_state, State, Width, Rounds}, Data) when is_binary(Data) ->
 	lists:all(fun(HashValue) ->
 		getBit(State, HashValue)
 	end, hash_bits(Width, Data, lists:seq(1, Rounds)));
@@ -56,14 +54,6 @@ exists(State, Data) when not is_binary(Data) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
-
-do_add(State, Data, Width, 1) ->
-	HashValue = hash_bit(Width, Data, <<1>>),
-	setBit(State, HashValue);
-do_add(State, Data, Width, N) ->
-	HashValue = hash_bit(Width, Data, <<N>>),
-	NewState  = setBit(State, HashValue),
-	do_add(NewState, Data, Width, N-1).
 
 
 nearest_block_size(Length) when is_float(Length) -> nearest_block_size(trunc(Length));
@@ -76,33 +66,13 @@ getBit(Bin, N)->
 		<<_:N/bits, 1:1, _/bits>> -> true
 	end.
 
-setBit(Bin, N)->
-	case Bin of
-		<<_:N/bits,1:1,_/bits>> -> Bin;
-		<<A:N/bits,0:1,B/bits>> -> <<A:N/bits,1:1,B/bits>>
-	end.
-
-hash_bit(Width, Data, Taint) ->
-	erlang:crc32(<<Data/binary, Taint/binary>>) rem Width.
-
 hash_bits(Width, Data, Taints) ->
 	MainHash = erlang:crc32(Data),
-	[erlang:crc32(MainHash, <<Taint>>) rem Width || Taint <- Taints].
-
-compute_deltas(_Offset, [], Acc) -> lists:reverse(Acc);
-compute_deltas(Offset, [Item |Rest], Acc) ->
-	compute_deltas(Item, Rest, [Item - Offset | Acc]).
+	lists:sort([erlang:crc32(MainHash, <<Taint>>) rem Width || Taint <- Taints]).
 
 setBits(Bin, []) -> Bin;
 setBits(Bin, [Offset |Rest]) ->
 	case Bin of
 		<<_:Offset/bits,1:1,_/bits>> -> setBits(Bin, Rest);
-		<<A:Offset/bits,0:1,B/bits>> -> <<A/bits, 1:1, (setBits(B, [ N - (Offset+1) || N <- Rest]))/bits>>
-	end.
-
-checkBits(_Bin, []) -> true;
-checkBits(Bin, [Offset |Rest]) ->
-	case Bin of
-		<<_:Offset/bits, 0:1, _/bits>> -> false;
-		<<_:Offset/bits, 1:1, B/bits>> -> checkBits(B, Rest)
+		<<A:Offset/bits,0:1,B/bits>> -> setBits(<<A:Offset/bits,1:1,B/bits>>, Rest)
 	end.
